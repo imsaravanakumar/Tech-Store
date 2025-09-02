8417aa72cf2017458e1321c338adafbc49d13e19
Saravana Kumar <144697260+imsaravanakumar@users.noreply.github.com>
2025-09-02 15:56:44 +0530

    Update authService.js

--- frontend/src/services/authService.js
+++ frontend/src/services/authService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "https://tech-store-mern-stack.onrender.com/api/auth";
+const BASE_URL = "https://tech-store-eied.onrender.com/api/auth";
 
 export const signup = (data) => axios.post(`${BASE_URL}/signup`, data);
 
